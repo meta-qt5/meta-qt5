meta-qt5/meta-qt5

commit 0717c65abc24d67887c93b773129156f714d033b

cmake_qt5: prepend the Qt5 specific CMake variables to EXTRA_OECMAKE
Using the prepend function instead of "=+" makes sure the variables
are set even if the user overrides EXTRA_OECMAKE with "=" instead of just
appending to it.

Prepending also allows overriding because later arguments take precedence
ever earlier ones in CMake.

Signed-off-by: Pascal Bach <pascal.bach@siemens.com>
Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

classes/cmake_qt5.bbclass
@@ -3,7 +3,7 @@ inherit qmake5_paths
 
 DEPENDS_prepend = "qtbase "
 
-EXTRA_OECMAKE += " \
+EXTRA_OECMAKE_prepend = " \
     -DOE_QMAKE_PATH_PREFIX=${OE_QMAKE_PATH_PREFIX} \
     -DOE_QMAKE_PATH_HEADERS=${OE_QMAKE_PATH_HEADERS} \
     -DOE_QMAKE_PATH_LIBS=${OE_QMAKE_PATH_LIBS} \

commit 9606c517ec4d411d13d001f94d6fea34eb71f7e7

cmake_qt5.bbclass: when using _prepend don't forget to add space at the end
Previous commit 0717c65abc24d67887c93b773129156f714d033b introduced a bug by
using _prepend w/o a space at the end, resulting in corrupted EXTRA_OEMAKE
variable in packages that inherit cmake_qt5.bbclass.

Signed-off-by: Denys Dmytriyenko <denys@ti.com>
Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

classes/cmake_qt5.bbclass
@@ -32,4 +32,5 @@ EXTRA_OECMAKE_prepend = " \
     -DOE_QMAKE_PATH_QT_DOCS=${OE_QMAKE_PATH_QT_DOCS} \
     -DOE_QMAKE_PATH_QT_SETTINGS=${OE_QMAKE_PATH_QT_SETTINGS} \
     -DOE_QMAKE_PATH_QT_EXAMPLES=${OE_QMAKE_PATH_QT_EXAMPLES} \
-    -DOE_QMAKE_PATH_QT_TESTS=${OE_QMAKE_PATH_QT_TESTS}"
+    -DOE_QMAKE_PATH_QT_TESTS=${OE_QMAKE_PATH_QT_TESTS} \
+"