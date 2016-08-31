meta-qt5/meta-qt5

commit d6aeedb7f27b8dd02fa7e6fa193ce9e2e969ecf6

qtbase: Fix installation of fonts and libraries
This patch fixes three problems
    1) Running on a case-sensitive system would result in an error
       due to invalid casing of pattern to install.
    2) Using install on symlinks would cause multiple copies of the
       same file to be installed on the target-system, since install
       doesn't preserve symlinks. This wastes a lot of space and it
       causes ldconfig to complain about non-symlinked files.
    3) Leakage of user-ids, i.e. cp -a would preserve the uid/gid of
       whoever built the files, which is usually not what you want.

Signed-off-by: Simon Aittamaa <simon.aittamaa@gmail.com>
Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

recipes-qt/qt5/qtbase_git.bb
@@ -209,10 +209,10 @@ do_install_append() {
     # install fonts manually if they are missing
     if [ ! -d ${D}/${OE_QMAKE_PATH_QT_FONTS} ]; then
         mkdir -p ${D}/${OE_QMAKE_PATH_QT_FONTS}
-        cp -a ${S}/lib/fonts/* ${D}/${OE_QMAKE_PATH_QT_FONTS}
+        cp -d ${S}/lib/fonts/* ${D}/${OE_QMAKE_PATH_QT_FONTS}
         chown -R root:root ${D}/${OE_QMAKE_PATH_QT_FONTS}
     fi
-    install -m 0644 ${B}/lib/libqt* ${D}${libdir}
+    cp -d ${B}/lib/libQt* ${D}${libdir}
     # Remove example.pro file as it is useless
     rm -f ${D}${OE_QMAKE_PATH_EXAMPLES}/examples.pro
 

commit a8d4dafdfde812e403315d53b2a981d12bd49f04

qtbase: fix regression for missing libqt*.a libs
Deploy the following statically linked libraries:
../build/lib/libqtfreetype.a  ../build/lib/libqtfreetype.prl
../build/lib/libqtpcre.a  ../build/lib/libqtpcre.prl

Fixes build of qtwayland (which is required for meta-toolchain-qt5):
| .../arm-angstrom-linux-gnueabi-ld.gold: error: cannot find -lqtfreetype
regression introduced with d6aeedb7f27b8dd02fa7e6fa193ce9e2e969ecf6 which
reverted the fix by Khem c44912563c4eadc288050f0b5e25981bc432d3f7

Signed-off-by: Max Krummenacher <max.krummenacher@toradex.com>
Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

recipes-qt/qt5/qtbase_git.bb
@@ -232,7 +232,7 @@ do_install_append() {
         cp -d ${S}/lib/fonts/* ${D}/${OE_QMAKE_PATH_QT_FONTS}
         chown -R root:root ${D}/${OE_QMAKE_PATH_QT_FONTS}
     fi
-    cp -d ${B}/lib/libQt* ${D}${libdir}
+    cp -d ${B}/lib/libqt* ${D}${libdir}
     # Remove example.pro file as it is useless
     rm -f ${D}${OE_QMAKE_PATH_EXAMPLES}/examples.pro
 