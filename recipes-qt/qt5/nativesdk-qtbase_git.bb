meta-qt5/meta-qt5

commit efa8aaf82e580a7d32eaaab48eb92d436f2e222a

qmake5_base.bbclass: set qt.conf by environment variable again
Qt update to 5.8 caused fallout in meta-qt5-extra. It turned out that
qmake pulled in many pointers to native sysroot. Native sysroot is qmake's
default in case no configuration file qt.conf is set.
The problem with the new way of setting qt.conf by cmd parameter is that is has
to be set explicitly. For instance all the packages coming from [1] use
autotools for configuration. These ask qmake for paths during configuration
and build further Makefiles during compile. To patch these for additional cmd
parameter is nasty business.
Setting qt.conf when building all qtbase's causes errors during configuration.
To fix this we set the filename to a non existent file so that Qt's default is
used. This dirty hack was necessary because there was no way found to export
an empty variable OE_QMAKE_QTCONF_PATH.

This patch was tested by building huge parts of meta-qt5-extra. The meta-layers
did not yet contain oe-core's recipe-specific-sysroot changes so master build
might fail for missing dependencies.

[1] http://www.rncbc.org/drupal/

Signed-off-by: Andreas Müller <schnitzeltony@googlemail.com>
Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

recipes-qt/qt5/nativesdk-qtbase_git.bb
@@ -27,6 +27,7 @@ FILESEXTRAPATHS =. "${FILE_DIRNAME}/qtbase:"
 # common for qtbase-native, qtbase-nativesdk and qtbase
 SRC_URI += "\
     file://0001-Add-linux-oe-g-platform.patch \
+    file://0002-qlibraryinfo-allow-to-set-qt.conf-from-the-outside-u.patch \
     file://0003-Add-external-hostbindir-option.patch \
     file://0004-qt_module-Fix-pkgconfig-and-libtool-replacements.patch \
     file://0005-configure-bump-path-length-from-256-to-512-character.patch \
@@ -91,6 +92,10 @@ QT_CONFIG_FLAGS += " \
 # qtbase is exception, as these are used as install path for sysroots
 OE_QMAKE_PATH_HOST_DATA = "${libdir}${QT_DIR_NAME}"
 OE_QMAKE_PATH_HOST_LIBS = "${libdir}"
+
+# for qtbase configuration we need default settings
+# since we cannot set empty set filename to a not existent file
+export OE_QMAKE_QTCONF_PATH = "foodummy"
 
 do_configure() {
     ${S}/configure -v \

recipes-qt/qt5/qtbase-native_git.bb
@@ -22,6 +22,7 @@ require qt5-git.inc
 # common for qtbase-native, qtbase-nativesdk and qtbase
 SRC_URI += "\
     file://0001-Add-linux-oe-g-platform.patch \
+    file://0002-qlibraryinfo-allow-to-set-qt.conf-from-the-outside-u.patch \
     file://0003-Add-external-hostbindir-option.patch \
     file://0004-qt_module-Fix-pkgconfig-and-libtool-replacements.patch \
     file://0005-configure-bump-path-length-from-256-to-512-character.patch \
@@ -76,6 +77,10 @@ PACKAGECONFIG_CONFARGS = " \
     -no-rpath \
     -platform linux-oe-g++ \
 "
+
+# for qtbase configuration we need default settings
+# since we cannot set empty set filename to a not existent file
+export OE_QMAKE_QTCONF_PATH = "foodummy"
 
 do_configure_prepend() {
     MAKEFLAGS="${PARALLEL_MAKE}" ${S}/configure -opensource -confirm-license ${PACKAGECONFIG_CONFARGS} || die "Configuring qt failed. PACKAGECONFIG_CONFARGS was ${PACKAGECONFIG_CONFARGS}"

recipes-qt/qt5/qtbase_git.bb
@@ -17,6 +17,7 @@ LIC_FILES_CHKSUM = " \
 # common for qtbase-native, qtbase-nativesdk and qtbase
 SRC_URI += "\
     file://0001-Add-linux-oe-g-platform.patch \
+    file://0002-qlibraryinfo-allow-to-set-qt.conf-from-the-outside-u.patch \
     file://0003-Add-external-hostbindir-option.patch \
     file://0004-qt_module-Fix-pkgconfig-and-libtool-replacements.patch \
     file://0005-configure-bump-path-length-from-256-to-512-character.patch \
@@ -128,6 +129,10 @@ QT_CONFIG_FLAGS += " \
     -pkg-config \
     ${PACKAGECONFIG_CONFARGS} \
 "
+
+# for qtbase configuration we need default settings
+# since we cannot set empty set filename to a not existent file
+export OE_QMAKE_QTCONF_PATH = "foodummy"
 
 do_configure() {
     # Avoid qmake error "Cannot read [...]/usr/lib/qt5/mkspecs/oe-device-extra.pri: No such file or directory" during configuration

classes/qmake5_base.bbclass
@@ -41,8 +41,9 @@ export OE_QMAKE_LDFLAGS = "${LDFLAGS}"
 export OE_QMAKE_AR = "${AR}"
 export OE_QMAKE_STRIP = "echo"
 export OE_QMAKE_WAYLAND_SCANNER = "${STAGING_BINDIR_NATIVE}/wayland-scanner"
-OE_QMAKE_QTCONF_PATH = "${WORKDIR}/qt.conf"
-export OE_QMAKE_QTCONF = "-qtconf ${OE_QMAKE_QTCONF_PATH}"
+
+# qmake reads if from shell environment
+export OE_QMAKE_QTCONF_PATH = "${WORKDIR}/qt.conf"
 
 inherit qmake5_paths remove-libtool
 
@@ -170,8 +171,8 @@ qmake5_base_do_configure () {
     # for config.tests to read this
     export QMAKE_MAKE_ARGS="${EXTRA_OEMAKE}"
 
-    CMD="${OE_QMAKE_QMAKE} -makefile -o Makefile ${OE_QMAKE_QTCONF} ${OE_QMAKE_DEBUG_OUTPUT} ${OE_QMAKE_RECURSIVE} $QMAKE_VARSUBST_PRE $AFTER $PROFILES $QMAKE_VARSUBST_POST -- $QMAKE_VARSUBST_CONFIGURE"
-    ${OE_QMAKE_QMAKE} -makefile -o Makefile ${OE_QMAKE_QTCONF} ${OE_QMAKE_DEBUG_OUTPUT} ${OE_QMAKE_RECURSIVE} $QMAKE_VARSUBST_PRE $AFTER $PROFILES $QMAKE_VARSUBST_POST -- $QMAKE_VARSUBST_CONFIGURE || die "Error calling $CMD"
+    CMD="${OE_QMAKE_QMAKE} -makefile -o Makefile ${OE_QMAKE_DEBUG_OUTPUT} ${OE_QMAKE_RECURSIVE} $QMAKE_VARSUBST_PRE $AFTER $PROFILES $QMAKE_VARSUBST_POST -- $QMAKE_VARSUBST_CONFIGURE"
+    ${OE_QMAKE_QMAKE} -makefile -o Makefile ${OE_QMAKE_DEBUG_OUTPUT} ${OE_QMAKE_RECURSIVE} $QMAKE_VARSUBST_PRE $AFTER $PROFILES $QMAKE_VARSUBST_POST -- $QMAKE_VARSUBST_CONFIGURE || die "Error calling $CMD"
 }
 
 qmake5_base_native_do_install() {

recipes-qt/qt5/qtbase/0002-qlibraryinfo-allow-to-set-qt.conf-from-the-outside-u.patch
@@ -1,0 +1,38 @@
+From e5f8607cfa1d2bcc798686cdf7c87ea0c6577d30 Mon Sep 17 00:00:00 2001
+From: Holger Freyther <zecke@selfish.org>
+Date: Wed, 26 Sep 2012 17:22:30 +0200
+Subject: [PATCH] qlibraryinfo: allow to set qt.conf from the outside using the
+ environment
+
+Allow to set a qt.conf from the outside using the environment. This allows
+to inject new prefixes and other paths into qmake. This is needed when using
+the same qmake binary to build qt/x11 and qt/embedded
+
+Upstream-Status: Inappropriate [embedded specific]
+  again very OE specific to read everything from environment (reusing the same
+  qmake from sstate and replacing all configured paths in it with qt.conf from
+  environment).
+
+Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>
+---
+ src/corelib/global/qlibraryinfo.cpp | 5 ++++-
+ 1 file changed, 4 insertions(+), 1 deletion(-)
+
+diff --git a/src/corelib/global/qlibraryinfo.cpp b/src/corelib/global/qlibraryinfo.cpp
+index 27fe10a..29ffcda 100644
+--- a/src/corelib/global/qlibraryinfo.cpp
++++ b/src/corelib/global/qlibraryinfo.cpp
+@@ -164,7 +164,10 @@ void QLibrarySettings::load()
+ QSettings *QLibraryInfoPrivate::findConfiguration()
+ {
+ #ifdef QT_BUILD_QMAKE
+-    QString qtconfig = qmake_libraryInfoFile();
++    QByteArray config = getenv("OE_QMAKE_QTCONF_PATH");
++    QString qtconfig = QFile::decodeName(config);
++    if(!QFile::exists(qtconfig))
++        qtconfig = qmake_libraryInfoFile();
+     if (QFile::exists(qtconfig))
+         return new QSettings(qtconfig, QSettings::IniFormat);
+ #else
+-- 
+2.9.3

recipes-qt/qt5/qtwebengine_git.bb
@@ -78,7 +78,7 @@ do_configure() {
 
     # qmake can't find the OE_QMAKE_* variables on it's own so directly passing them as
     # arguments here
-    ${OE_QMAKE_QMAKE} ${OE_QMAKE_QTCONF} -r ${EXTRA_QMAKEVARS_PRE} ${S} \
+    ${OE_QMAKE_QMAKE} -r ${EXTRA_QMAKEVARS_PRE} ${S} \
         QMAKE_CXX="${OE_QMAKE_CXX}" QMAKE_CC="${OE_QMAKE_CC}" \
         QMAKE_LINK="${OE_QMAKE_LINK}" \
         QMAKE_CFLAGS="${OE_QMAKE_CFLAGS}" \

commit a17ff281aa8d99f770b0a049cb798235005fb93e

qtbase: fix Krogoth build regression from efa8aaf
With

    commit efa8aaf82e580a7d32eaaab48eb92d436f2e222a
    Author: Andreas Müller <schnitzeltony@googlemail.com>
    Date:   Thu Feb  9 00:26:09 2017

    qmake5_base.bbclass: set qt.conf by environment variable again

we stopped pointing ${OE_QMAKE_QTCONF_PATH} at a valid file and
instead directed it to a path which was intended not to exist.

The motivation was to permit qtbase/qtbase-native/nativesdk-qtbase
to build again after Qt 5.8 started paying attention to the
contents of this file.

The change as done in efa8aaf works well enough for Morty and
subsequent releases' copies of Bitbake, but fails on earlier
releases because they lack the following change:

    commit 2afcbfef2cd1ca568e5225884a8021df38ee3db0
    Author: Ross Burton <ross.burton@intel.com>
    Date: 2016-07-14 13:56:22

    bitbake: build: don't use $B as the default cwd for functions

The result is that when we build with Krogoth or prior, the body
of do_generate_qt_config_file() runs with a cwd of ${B}, which was
_not_ the intent of efa8aaf. Because the working directory is ${B},
${OE_QMAKE_QTCONF_PATH} is written in there too. do_configure() --
whose cwd is by design also ${B} -- then finds the file 'foodummy',
and the build breaks for the reasons outlined in efa8aaf.

This change simply shifts the implementation tactics to suppress
the creation of ${OE_QMAKE_QTCONF_PATH} during qtbase rather than rely
on unspecified behavior about the cwd of do_generate_qt_config_file().

Change-Id: I93596ed78cba628d3b673c4fbbcff585e04d4402
Reviewed-by: Samuli Piippo <samuli.piippo@qt.io>

recipes-qt/qt5/nativesdk-qtbase_git.bb
@@ -93,7 +93,7 @@ OE_QMAKE_PATH_HOST_LIBS = "${libdir}"
 
 # for qtbase configuration we need default settings
 # since we cannot set empty set filename to a not existent file
-export OE_QMAKE_QTCONF_PATH = "foodummy"
+deltask generate_qt_config_file
 
 do_configure() {
     ${S}/configure -v \

recipes-qt/qt5/qtbase-native_git.bb
@@ -79,7 +79,7 @@ PACKAGECONFIG_CONFARGS = " \
 
 # for qtbase configuration we need default settings
 # since we cannot set empty set filename to a not existent file
-export OE_QMAKE_QTCONF_PATH = "foodummy"
+deltask generate_qt_config_file
 
 do_configure_prepend() {
     # Avoid qmake error "Cannot read [...]/usr/lib/qt5/mkspecs/oe-device-extra.pri: No such file or directory"

recipes-qt/qt5/qtbase_git.bb
@@ -129,7 +129,7 @@ QT_CONFIG_FLAGS += " \
 
 # for qtbase configuration we need default settings
 # since we cannot set empty set filename to a not existent file
-export OE_QMAKE_QTCONF_PATH = "foodummy"
+deltask generate_qt_config_file
 
 do_configure() {
     # Avoid qmake error "Cannot read [...]/usr/lib/qt5/mkspecs/oe-device-extra.pri: No such file or directory" during configuration