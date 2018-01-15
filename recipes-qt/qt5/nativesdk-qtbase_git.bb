meta-qt5/meta-qt5

commit b8a236df79c25802c1f2eb2525e8a534e73e608e

qt5: Fix cmake config files
* without syncqt cmake config files have empty PRIVATE_INCLUDE_DIRS
  remove pregenerated header files and create .git dir to let qmake
  call syncqt and generate headers like when building from git recipes

Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

recipes-qt/qt5/qt5-native.inc
@@ -10,3 +10,10 @@ OE_QMAKE_PATH_DOCS = "${OE_QMAKE_PATH_QT_DOCS}"
 OE_QMAKE_PATH_SETTINGS = "${OE_QMAKE_PATH_QT_SETTINGS}"
 OE_QMAKE_PATH_EXAMPLES = "${OE_QMAKE_PATH_QT_EXAMPLES}"
 OE_QMAKE_PATH_TESTS = "${OE_QMAKE_PATH_QT_TESTS}"
+
+# Regenerate header files when they are included in source tarball
+# Otherwise cmake files don't set PRIVATE_HEADERS correctly
+do_configure_prepend() {
+    rm -rf ${S}/include
+    mkdir -p ${S}/.git || true
+}

recipes-qt/qt5/qt5.inc
@@ -32,6 +32,14 @@ python __anonymous() {
             d.setVar("PACKAGE_ARCH", tarch)
 }
 
+
+# Regenerate header files when they are included in source tarball
+# Otherwise cmake files don't set PRIVATE_HEADERS correctly
+do_configure_prepend() {
+    rm -rf ${S}/include
+    mkdir -p ${S}/.git || true
+}
+
 PACKAGES =. "${PN}-qmlplugins-dbg ${PN}-tools-dbg ${PN}-plugins-dbg ${PN}-qmlplugins ${PN}-tools ${PN}-plugins ${PN}-mkspecs ${PN}-examples-dev ${PN}-examples-staticdev ${PN}-examples-dbg ${PN}-examples "
 
 # extra -dbg packages

recipes-qt/qt5/qtbase-native_git.bb
@@ -1,27 +1,6 @@
 require qt5-git.inc
 require ${PN}.inc
 
-# common with target version
-SRC_URI = "\
-    git://qt.gitorious.org/qt/${QT_MODULE}.git;branch=${QT_MODULE_BRANCH} \
-    file://0001-Add-linux-oe-g-platform.patch \
-    file://0002-qlibraryinfo-allow-to-set-qt.conf-from-the-outside-u.patch \
-    file://0003-Add-external-hostbindir-option.patch \
-    file://0004-qt_functions-temporary-remove-isEmpty-check.patch \
-    file://0005-configureapp-Prefix-default-LIBDIRS-and-INCDIRS-with.patch \
-    file://0006-qt_module-Fix-pkgconfig-replacement.patch \
-    file://0007-qt_module-Fix-paths-in-.prl-files.patch \
-    file://0008-wayland-scanner-disable-silent-rules.patch \
-    file://0009-configure-don-t-export-SYSTEM_VARIABLES-to-.qmake.va.patch \
-    file://0010-configure.prf-Allow-to-add-extra-arguments-to-make.patch \
-    file://0011-configure-make-pulseaudio-a-configurable-option.patch \
-    file://0012-configure-make-alsa-a-configurable-option.patch \
-    file://0013-configure-make-freetype-a-configurable-option.patch \
-    file://0014-Use-OE_QMAKE_PATH_EXTERNAL_HOST_BINS-to-determine-pa.patch \
-"
-
-SRC_URI += "file://0015-Always-build-uic.patch"
-
 do_install_append() {
     # for modules which are still using syncqt and call qtPrepareTool(QMAKE_SYNCQT, syncqt)
     # e.g. qt3d, qtwayland

commit 34a3a4bf4b58ea7e63ff81eaf35963cbd4cf59b5

qtbase: fix a regression
A flaw was introduced by commit b8a236df:
[ qt5: Fix cmake config files ]

Removing ${S}/include must be done before configure script being
called, but it's not in that case for qtbase-native recipe after
the commit, that is saying, ${S}/include was removed after configure
being called, which causes compiling issues for native build.

Further more, the logic introduced by commit b8a236df should only apply
to qtbase recipes, so they should be moved to qtbase recipes as well.

Signed-off-by: Ming Liu <liu.ming50@gmail.com>
Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

recipes-qt/qt5/nativesdk-qtbase_git.bb
@@ -102,6 +102,11 @@ OE_QMAKE_PATH_HOST_LIBS = "${libdir}"
 deltask generate_qt_config_file
 
 do_configure() {
+    # Regenerate header files when they are included in source tarball
+    # Otherwise cmake files don't set PRIVATE_HEADERS correctly
+    rm -rf ${S}/include
+    mkdir -p ${S}/.git || true
+
     ${S}/configure -v \
         -${QT_EDITION} -confirm-license \
         -sysroot ${STAGING_DIR_TARGET} \

recipes-qt/qt5/qt5-native.inc
@@ -10,10 +10,3 @@ OE_QMAKE_PATH_DOCS = "${OE_QMAKE_PATH_QT_DOCS}"
 OE_QMAKE_PATH_SETTINGS = "${OE_QMAKE_PATH_QT_SETTINGS}"
 OE_QMAKE_PATH_EXAMPLES = "${OE_QMAKE_PATH_QT_EXAMPLES}"
 OE_QMAKE_PATH_TESTS = "${OE_QMAKE_PATH_QT_TESTS}"
-
-# Regenerate header files when they are included in source tarball
-# Otherwise cmake files don't set PRIVATE_HEADERS correctly
-do_configure_prepend() {
-    rm -rf ${S}/include
-    mkdir -p ${S}/.git || true
-}

recipes-qt/qt5/qt5.inc
@@ -35,13 +35,6 @@ python __anonymous() {
             d.setVar("PACKAGE_ARCH", tarch)
 }
 
-# Regenerate header files when they are included in source tarball
-# Otherwise cmake files don't set PRIVATE_HEADERS correctly
-do_configure_prepend() {
-    rm -rf ${S}/include
-    mkdir -p ${S}/.git || true
-}
-
 # Many examples come with libraries installed outside of standard libdir,
 # suppress QA check complaining
 INSANE_SKIP_${PN}-dbg += "libdir"

recipes-qt/qt5/qtbase-native_git.bb
@@ -100,6 +100,11 @@ PACKAGECONFIG_CONFARGS = " \
 deltask generate_qt_config_file
 
 do_configure_prepend() {
+    # Regenerate header files when they are included in source tarball
+    # Otherwise cmake files don't set PRIVATE_HEADERS correctly
+    rm -rf ${S}/include
+    mkdir -p ${S}/.git || true
+
     # Avoid qmake error "Cannot read [...]/usr/lib/qt5/mkspecs/oe-device-extra.pri: No such file or directory"
     touch ${S}/mkspecs/oe-device-extra.pri
 

recipes-qt/qt5/qtbase_git.bb
@@ -167,6 +167,11 @@ XPLATFORM_toolchain-clang = "linux-oe-clang"
 XPLATFORM ?= "linux-oe-g++"
 
 do_configure() {
+    # Regenerate header files when they are included in source tarball
+    # Otherwise cmake files don't set PRIVATE_HEADERS correctly
+    rm -rf ${S}/include
+    mkdir -p ${S}/.git || true
+
     # Avoid qmake error "Cannot read [...]/usr/lib/qt5/mkspecs/oe-device-extra.pri: No such file or directory" during configuration
     touch ${S}/mkspecs/oe-device-extra.pri
 