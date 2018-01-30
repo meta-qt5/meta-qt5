meta-qt5/meta-qt5

commit e8ec89bf1724836b054e8629e5ad90eb4fd97868

qt: refresh remaining patches and add links to meta-qt5 repos
* add comment with link to meta-qt5 repository where the meta-qt5 .patch
  files are maintained, include branch and tag name so that it's more
  clear from where these changes are exported (with:
  git format-patch --no-numbered --no-signature)

Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

recipes-qt/qt5/qtwebengine_git.bb
@@ -107,6 +107,8 @@ RDEPENDS_${PN}-examples += " \
 
 QT_MODULE_BRANCH_CHROMIUM = "49-based"
 
+# Patches from https://github.com/meta-qt5/qtwebengine/commits/b5.6
+# 5.6.meta-qt5.1
 SRC_URI += " \
     ${QT_GIT}/qtwebengine-chromium.git;name=chromium;branch=${QT_MODULE_BRANCH_CHROMIUM};destsuffix=git/src/3rdparty \
     file://0001-functions.prf-Don-t-match-QMAKE_EXT_CPP-or-QMAKE_EXT.patch \
@@ -114,13 +116,16 @@ SRC_URI += " \
     file://0003-functions.prf-allow-build-for-linux-oe-g-platform.patch \
     file://0004-WebEngine-qquickwebengineview_p_p.h-add-include-QCol.patch \
     file://0005-Include-dependency-to-QCoreApplication-translate.patch \
-    file://0001-chromium-base.gypi-include-atomicops_internals_x86_g.patch \
-    file://0002-chromium-Change-false-to-FALSE-and-1-to-TRUE-FIX-qtw.patch \
+"
+
+# Patches from https://github.com/meta-qt5/qtwebengine-chromium/commits/49-based
+# 49-based.meta-qt5.1
+SRC_URI += " \
+    file://0001-chromium-base.gypi-include-atomicops_internals_x86_g.patch;patchdir=src/3rdparty \
+    file://0002-chromium-Change-false-to-FALSE-and-1-to-TRUE-FIX-qtw.patchh;patchdir=src/3rdparty \
 "
 
 SRCREV_qtwebengine = "fad625e0ba39e855817bbf206ab9a846d07aeeec"
-# This is in git submodule, but we're using latest in 45-based
-# SRCREV_chromium = "79930a541473b2e0f950d040c16ab6f22e4aeef3"
 SRCREV_chromium = "cb094c05c5f06489fa64412e7f5d9e194a3f9495"
 SRCREV = "${SRCREV_qtwebengine}"
 

recipes-qt/qt5/nativesdk-qtbase_git.bb
@@ -22,6 +22,8 @@ require qt5-git.inc
 FILESEXTRAPATHS =. "${FILE_DIRNAME}/qtbase:"
 
 # common for qtbase-native, qtbase-nativesdk and qtbase
+# Patches from https://github.com/meta-qt5/qtbase/commits/b5.6-shared
+# 5.6.meta-qt5-shared.9
 SRC_URI += "\
     file://0001-Add-linux-oe-g-platform.patch \
     file://0002-qlibraryinfo-allow-to-set-qt.conf-from-the-outside-u.patch \
@@ -35,6 +37,8 @@ SRC_URI += "\
 "
 
 # common for qtbase-native and nativesdk-qtbase
+# Patches from https://github.com/meta-qt5/qtbase/commits/b5.6-native
+# 5.6.meta-qt5-native.9
 SRC_URI += " \
     file://0010-Always-build-uic.patch \
     file://0011-Add-external-hostbindir-option-for-native-sdk.patch \

recipes-qt/qt5/qt3d/0001-Allow-a-tools-only-build.patch
@@ -1,4 +1,4 @@
-From 339e527e5ca0e1b7839d28e9f140630af634b569 Mon Sep 17 00:00:00 2001
+From e46d749532d1ca5f74d5c4d8d690972675e5c638 Mon Sep 17 00:00:00 2001
 From: Samuli Piippo <samuli.piippo@theqtcompany.com>
 Date: Wed, 10 Feb 2016 09:02:09 +0200
 Subject: [PATCH] Allow a tools-only build

recipes-qt/qt5/qt3d_git.bb
@@ -11,6 +11,8 @@ LIC_FILES_CHKSUM = " \
 DEPENDS += "qtbase"
 DEPENDS_class-target += "qtdeclarative qt3d-native"
 
+# Patches from https://github.com/meta-qt5/qt3d/commits/b5.6
+# 5.6.meta-qt5.1
 SRC_URI += " \
     file://0001-Allow-a-tools-only-build.patch \
 "

recipes-qt/qt5/qtbase-native_git.bb
@@ -15,6 +15,8 @@ require qt5-native.inc
 require qt5-git.inc
 
 # common for qtbase-native, qtbase-nativesdk and qtbase
+# Patches from https://github.com/meta-qt5/qtbase/commits/b5.6-shared
+# 5.6.meta-qt5-shared.9
 SRC_URI += "\
     file://0001-Add-linux-oe-g-platform.patch \
     file://0002-qlibraryinfo-allow-to-set-qt.conf-from-the-outside-u.patch \
@@ -28,6 +30,8 @@ SRC_URI += "\
 "
 
 # common for qtbase-native and nativesdk-qtbase
+# Patches from https://github.com/meta-qt5/qtbase/commits/b5.6-native
+# 5.6.meta-qt5-native.9
 SRC_URI += " \
     file://0010-Always-build-uic.patch \
     file://0011-Add-external-hostbindir-option-for-native-sdk.patch \

recipes-qt/qt5/qtbase/0001-Add-linux-oe-g-platform.patch
@@ -1,4 +1,4 @@
-From 46260d1a93e10a6a6f47f38d0b4355b4b63a4682 Mon Sep 17 00:00:00 2001
+From 5d1bf6c3e826af7282b858c192c62aad77095fc0 Mon Sep 17 00:00:00 2001
 From: Martin Jansa <Martin.Jansa@gmail.com>
 Date: Mon, 15 Apr 2013 04:29:32 +0200
 Subject: [PATCH] Add linux-oe-g++ platform
@@ -56,7 +56,7 @@ Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>
  create mode 100644 mkspecs/linux-oe-g++/qplatformdefs.h
 
 diff --git a/configure b/configure
-index f4c7813..9ab6a9d 100755
+index 6ebeab3..9e552c6 100755
 --- a/configure
 +++ b/configure
 @@ -343,6 +343,16 @@ getQMakeConf()

recipes-qt/qt5/qtbase/0002-qlibraryinfo-allow-to-set-qt.conf-from-the-outside-u.patch
@@ -1,4 +1,4 @@
-From 03824427f227ff16c6d23d08c73708b0faf98e06 Mon Sep 17 00:00:00 2001
+From a5e43264353bafc2ea4ad6813dbab6b271aee388 Mon Sep 17 00:00:00 2001
 From: Holger Freyther <zecke@selfish.org>
 Date: Wed, 26 Sep 2012 17:22:30 +0200
 Subject: [PATCH] qlibraryinfo: allow to set qt.conf from the outside using the
@@ -19,7 +19,7 @@ Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>
  1 file changed, 4 insertions(+), 1 deletion(-)
 
 diff --git a/src/corelib/global/qlibraryinfo.cpp b/src/corelib/global/qlibraryinfo.cpp
-index 8bcacca..e1c87b5 100644
+index 14be4c3..1bddb53 100644
 --- a/src/corelib/global/qlibraryinfo.cpp
 +++ b/src/corelib/global/qlibraryinfo.cpp
 @@ -157,7 +157,10 @@ void QLibrarySettings::load()

recipes-qt/qt5/qtbase/0003-Add-external-hostbindir-option.patch
@@ -1,4 +1,4 @@
-From 7421a689c695a09fff2d5ffbdbe56e34d73f771d Mon Sep 17 00:00:00 2001
+From 1e9313a736c9d381c70e723d8075c206bfaf47f5 Mon Sep 17 00:00:00 2001
 From: Martin Jansa <Martin.Jansa@gmail.com>
 Date: Sat, 6 Apr 2013 13:15:07 +0200
 Subject: [PATCH] Add -external-hostbindir option
@@ -37,7 +37,7 @@ Conflicts:
  7 files changed, 38 insertions(+), 12 deletions(-)
 
 diff --git a/configure b/configure
-index 9ab6a9d..225b9f3 100755
+index 9e552c6..ae724d6 100755
 --- a/configure
 +++ b/configure
 @@ -826,6 +826,7 @@ QT_HOST_BINS=
@@ -125,7 +125,7 @@ index 839c3d6..45934a0 100644
  
      !host_build|!force_bootstrap: MODULE_DEPENDS = $$replace(QT, -private$, _private)
 diff --git a/qtbase.pro b/qtbase.pro
-index 98ca86a..0eed475 100644
+index 78f9b3a..f4b7fc4 100644
 --- a/qtbase.pro
 +++ b/qtbase.pro
 @@ -37,12 +37,16 @@ CONFIG -= qt

commit bbedb361744160727d0c5f2344d90a9cf2d79db2

qtwebengine: Fix typo in patch filename
Fixes fetcher failure due to typo in patch filename introduced in
commit e8ec89bf1724836b054e8629e5ad90eb4fd97868

Signed-off-by: Niko Mauno <niko.mauno@vaisala.com>
Signed-off-by: Martin Jansa <Martin.Jansa@gmail.com>

recipes-qt/qt5/qtwebengine_git.bb
@@ -122,7 +122,7 @@ SRC_URI += " \
 # 49-based.meta-qt5.1
 SRC_URI += " \
     file://0001-chromium-base.gypi-include-atomicops_internals_x86_g.patch;patchdir=src/3rdparty \
-    file://0002-chromium-Change-false-to-FALSE-and-1-to-TRUE-FIX-qtw.patchh;patchdir=src/3rdparty \
+    file://0002-chromium-Change-false-to-FALSE-and-1-to-TRUE-FIX-qtw.patch;patchdir=src/3rdparty \
 "
 
 SRCREV_qtwebengine = "fad625e0ba39e855817bbf206ab9a846d07aeeec"