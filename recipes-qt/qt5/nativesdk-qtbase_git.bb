meta-qt5/meta-qt5

commit a28765bf60028e7dc59130432edf16782b77a438

Allow enabling gui for native and nativesdk
Some Qt build tools may depend on QtGui, which need platform plugins
for running them. Use minimal platform plugin since no interaction
with the tool is expected.

Add PACKAGECONFIGs that can be used to enable gui and image format
plugins for the native and nativesdk builds.

Task-number: AUTOSUITE-1009
Change-Id: I39a9c5612da2c84be44e55665a970ed2a2fd512f
Reviewed-by: Nikolay Zamotaev <nzamotaev@luxoft.com>
Reviewed-by: Mikko Gronoff <mikko.gronoff@qt.io>

recipes-qt/qt5/nativesdk-qtbase_git.bb
@@ -65,6 +65,7 @@ FILES_${PN}-dev += " \
 
 FILES_${PN} += " \
     ${SDKPATHNATIVE}/environment-setup.d \
+    ${OE_QMAKE_PATH_PLUGINS} \
 "
 
 # qttools binaries are placed in a subdir of bin in order to avoid
@@ -72,6 +73,10 @@ FILES_${PN} += " \
 # package, since it doesn't detect binaries in subdirs. Explicitly
 # disable package auto-renaming for the tools-package.
 DEBIAN_NOAUTONAME_${PN} = "1"
+
+PACKAGECONFIG ?= ""
+PACKAGECONFIG[gui] = "-gui -qpa minimal,-no-gui,"
+PACKAGECONFIG[imageformats] = "-qt-libpng -qt-libjpeg -gif -ico, -no-libpng -no-libjpg -no-ico -no-gif,"
 
 QT_CONFIG_FLAGS += " \
     -shared \
@@ -101,12 +106,8 @@ do_configure() {
         -no-gcc-sysroot \
         -system-zlib \
         -dbus-runtime \
-        -no-libjpeg \
-        -no-libpng \
-        -no-gif \
         -no-accessibility \
         -no-cups \
-        -no-gui \
         -no-sql-mysql \
         -no-sql-sqlite \
         -no-opengl \
@@ -148,7 +149,6 @@ do_install() {
 
     # remove things unused in nativesdk, we need the headers and libs
     rm -rf ${D}${datadir} \
-           ${D}/${OE_QMAKE_PATH_PLUGINS} \
            ${D}${libdir}/cmake \
            ${D}${libdir}/pkgconfig
 

recipes-qt/qt5/qtbase-native_git.bb
@@ -56,18 +56,18 @@ CLEANBROKEN = "1"
 XPLATFORM_toolchain-clang = "linux-oe-clang"
 XPLATFORM ?= "linux-oe-g++"
 
+PACKAGECONFIG ?= ""
+PACKAGECONFIG[gui] = "-gui -qpa minimal,-no-gui,"
+PACKAGECONFIG[imageformats] = "-qt-libpng -qt-libjpeg -gif -ico, -no-libpng -no-libjpg -no-ico -no-gif,"
+
 QT_CONFIG_FLAGS = " \
     -sysroot ${STAGING_DIR_NATIVE} \
     -L${STAGING_LIBDIR_NATIVE} \
     -no-gcc-sysroot \
     -system-zlib \
     -qt-pcre \
-    -no-libjpeg \
-    -no-libpng \
-    -no-gif \
     -no-accessibility \
     -no-cups \
-    -no-gui \
     -no-sql-mysql \
     -no-sql-sqlite \
     -no-sql-psql \

classes/qmake5_base.bbclass
@@ -110,6 +110,7 @@ generate_qt_config_file_effective_paths() {
     cat >> ${OE_QMAKE_QTCONF_PATH} <<EOF
 [EffectivePaths]
 HostBinaries = ${OE_QMAKE_PATH_EXTERNAL_HOST_BINS}
+HostLibraries = ${STAGING_LIBDIR_NATIVE}
 HostData = ${OE_QMAKE_PATH_HOST_DATA}
 HostPrefix = ${STAGING_DIR_NATIVE}
 EOF

commit 67a74e272da2b1fc3ea760181c1ec7837f53d19b

qtbase: fix typo in imageformats packageconfig
introduced by commit:
a28765bf60028e7dc59130432edf16782b77a438 Allow enabling gui for native and nativesdk

Change-Id: Id3a229592d0ed1cfbd8370d693790b6b2193fbde
Reviewed-by: Kari Oikarinen <kari.oikarinen@qt.io>

recipes-qt/qt5/nativesdk-qtbase_git.bb
@@ -76,7 +76,7 @@ DEBIAN_NOAUTONAME_${PN} = "1"
 
 PACKAGECONFIG ?= ""
 PACKAGECONFIG[gui] = "-gui -qpa minimal,-no-gui,"
-PACKAGECONFIG[imageformats] = "-qt-libpng -qt-libjpeg -gif -ico, -no-libpng -no-libjpg -no-ico -no-gif,"
+PACKAGECONFIG[imageformats] = "-qt-libpng -qt-libjpeg -gif -ico, -no-libpng -no-libjpeg -no-ico -no-gif,"
 
 QT_CONFIG_FLAGS += " \
     -shared \

recipes-qt/qt5/qtbase-native_git.bb
@@ -58,7 +58,7 @@ XPLATFORM ?= "linux-oe-g++"
 
 PACKAGECONFIG ?= ""
 PACKAGECONFIG[gui] = "-gui -qpa minimal,-no-gui,"
-PACKAGECONFIG[imageformats] = "-qt-libpng -qt-libjpeg -gif -ico, -no-libpng -no-libjpg -no-ico -no-gif,"
+PACKAGECONFIG[imageformats] = "-qt-libpng -qt-libjpeg -gif -ico, -no-libpng -no-libjpeg -no-ico -no-gif,"
 
 QT_CONFIG_FLAGS = " \
     -sysroot ${STAGING_DIR_NATIVE} \